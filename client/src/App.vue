20b5610cb66772e371e16432302740f157881667
Théo Letouzé <theo.letouze44@gmail.com>
2021-10-19 15:20:00 +0200

    vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Panel from './components/Panel.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="mainBackground"><Panel /></div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.mainBackground {
+  display: flex;
+  flex-direction: row-reverse;
+  background: url('./assets/background-storm.jpg') no-repeat center center fixed;
+  background-size: cover;
+  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
---
+++ client/src/components/Panel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="panel">
+    <p>{{ count }}</p>
+    <div class="cursor-pointer" @click="incCount">AUGMENTER</div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+const count = ref(0)
+function incCount() {
+  count.value++
+}
+</script>
+<style>
+.panel {
+  background-color: #484747;
+  opacity: 0.7;
+  width: 550px;
+}
+</style>
